83569ff2ac9362370d963a22415ea54579f560e4
admin <admin@feibendayudembp.lan>
2017-06-25 19:13:33 +0800

    Initial Commit

---
+++ WTSummary/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WTSummary
+//
+//  Created by admin on 17/6/25.
+//  Copyright © 2017年 飞奔的小鲨鱼. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ WTSummary/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WTSummary
+//
+//  Created by admin on 17/6/25.
+//  Copyright © 2017年 飞奔的小鲨鱼. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
